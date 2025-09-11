83b729eab8407a9f94257bcd05679526eb6c4433
taishin_gn <taishin_gn@albank.ru>
2025-09-11 21:46:25 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AdminPage from "./pages/AdminPage.vue";
+</script>
+
+<template>
+  <AdminPage />
+  asda И На русском
+</template>
+
+<style scoped lang="scss"></style>
