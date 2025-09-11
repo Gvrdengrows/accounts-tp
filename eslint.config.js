45498f7a9d0e37be57aafe7b27255339112d854d
taishin_gn <taishin_gn@albank.ru>
2025-09-11 22:32:17 +0900

    Add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,11 @@
 import pluginVue from "eslint-plugin-vue";
 import globals from "globals";
 
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+
 export default [
   ...pluginVue.configs["flat/recommended"],
+  ...eslintPluginPrettierRecommended,
   {
     rules: {
       // override/add rules settings here, such as:
